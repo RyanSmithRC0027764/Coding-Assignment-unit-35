09a9b9174dad67a57c2e7e030dbe34abd3f34310
Ryan Smith <rc0027764@student.riversidecollege.ac.uk>
2018-06-21 11:12:27 +0100

    Add project files.

---
+++ Coding Assignment unit 35/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Coding_Assignment_unit_35
+{
+    
+    public partial class Form1 : Form
+    {
+        class row
+        {
+            public double time;
+            public double altitude;
+            public double velocity;
+            public double accleration;
+        }
+        List<row> table = new List<row>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName ="";
+            openFileDialog1.Filter= "CSV file|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        string line = sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            table.Add(new row());
+                            string[] r = sr.ReadLine().Split(',');
+                            table.Last().time = double.Parse(r[0]);
+                            table.Last().altitude = double.Parse(r[1]);
+                        }
+                    }
+                    CalculateVelocity();
+                    CalculateAcceleration();
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "failed to open file.");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "is not in the required format.");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + "is not in the required format.");
+                }
+            }
+        }
+
+        private void velocityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            {
+                chart1.Series.Clear();
+                chart1.ChartAreas[0].AxisX.IsMarginVisible = false;
+                Series series = new Series
+                {
+                    Name = "velocity",
+                    Color = Color.Blue,
+                    IsVisibleInLegend = false,
+                    IsXValueIndexed = true,
+                    ChartType = SeriesChartType.Spline,
+                    BorderWidth = 2
+                };
+
+
+
+
+
+            }
+
+
+
+
+
